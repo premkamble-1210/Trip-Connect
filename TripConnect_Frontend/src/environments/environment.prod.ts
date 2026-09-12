bfa223bc188cfbae92c834f4af4b4a9e3a19a90f
premkamble-1210 <premkambl301@gmail.com>
2026-09-12 18:29:56 +0530

    docker improvement

---
+++ TripConnect_Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiBaseUrl: '',
+  openRouteServiceApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijk1NjgyYTQ0MzlhNzRlMjBiNzEzNDdjMjVjODYwYjY0IiwiaCI6Im11cm11cjY0In0=',
+};
